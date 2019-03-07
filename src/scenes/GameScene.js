16cf33741ad1487d6e1cd5fc777f4208bf412489
jufebrown <jufebrown@gmail.com>
2019-03-07 15:20:42 -0600

    change variable name for tilemap

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,18 +6,17 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('black-grey-tilemap', '../assets/tilesets/black-grey-tilemap.png');
+    this.load.image('blackGreyTilemap', '../assets/tilesets/black-grey-tilemap.png');
 
     const level = [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 1, 0, 1, 1, 1, 1, 0, 1, 0],
-      [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
       [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
       [0, 1, 0, 1, 1, 1, 1, 0, 1, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
 
     this.make.tilemap({ data: level, tilewidth: 32, tileheight: 32 });
-    this.addTilesetImage('black-grey-tilemap');
+    this.addTilesetImage('blackGreyTilemap');
   }
 }
